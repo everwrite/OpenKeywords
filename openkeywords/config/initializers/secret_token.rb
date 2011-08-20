6a4530e16a24082b98ffd6c5b8246210a62c3516
edmar <edmaroliveiraferreira@gmail.com>
2011-08-20 10:46:44 -0300

    Creating the rails project and organizing the template files

---
+++ openkeywords/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Openkeywords::Application.config.secret_token = '957b25562f61f6399b2131faa820a76168080c06d86c2fda730504e9384a3d4a59f79aa7d76c280bead87acb3a74c89bccbd5cc890ededa77c4d7d54b133bd38'
