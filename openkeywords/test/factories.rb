98efec87e03eea57d71153c99df89daead289bff
edmar <edmaroliveiraferreira@gmail.com>
2011-08-20 17:46:02 -0300

    Adding functional tests

---
+++ openkeywords/test/factories.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+   factory :user do |u|
+      u.email "edmaroliveiraferreira@gmail.com"
+      u.password "123456"
+   end
+end
--- openkeywords/test/functional/home_controller_test.rb
+++ openkeywords/test/functional/home_controller_test.rb
@@ -1,9 +1,13 @@
 require 'test_helper'
+require 'factory_girl'
 
 class HomeControllerTest < ActionController::TestCase
   test "should get index" do
+    @user = FactoryGirl.create(:user)
+    sign_in @user
     get :index
     assert_response :success
+    @user.delete
   end
 
 end
