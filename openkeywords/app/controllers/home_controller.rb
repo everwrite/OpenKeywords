c9a7c7fa6f83abe5b73671a102d2c8739a70fe57
edmar <edmaroliveiraferreira@gmail.com>
2011-10-02 18:24:57 -0300

    add search action

--- openkeywords/app/controllers/home_controller.rb
+++ openkeywords/app/controllers/home_controller.rb
@@ -1,8 +1,16 @@
 class HomeController < ApplicationController
+ 
   def index
-		if current_user.access_token.empty?
-			redirect_to "/access_token/new"
-		end
+	  if current_user
+		  if current_user.access_token.empty?
+			  redirect_to "/access_token/new"
+		  end
+	  end
+  end
+
+  def search
+    q = params[:q]
+    @keywords = KeywordDay.where( :t => /#{q}/i).order_by([[:v, :desc]]).page(params[:page]).per(20)
   end
 
 end
