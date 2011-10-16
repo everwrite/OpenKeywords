25513c82f5cda519e3f34a2cb5001babe609cdd7
edmar <edmaroliveiraferreira@gmail.com>
2011-10-16 16:36:44 -0200

    layout for devise

--- openkeywords/lib/exporter_job.rb
+++ openkeywords/lib/exporter_job.rb
@@ -1,11 +1,11 @@
 class ExporterJob
 	
 	def self.perform
-		system 'mongoexport  -h localhost -d openkeywords_development -c keyword_alls --csv -f t,c,v > /tmp/all.csv'
-	  system 'mongoexport  -h localhost -d openkeywords_development -c keyword_days --csv -f t,c,v > /tmp/days.csv'
-	  system 'mongoexport  -h localhost -d openkeywords_development -c keyword_weeks --csv -f t,c,v > /tmp/weeks.csv'
-	  system 'mongoexport  -h localhost -d openkeywords_development -c keyword_months --csv -f t,c,v > /tmp/months.csv'
-	  system 'mongoexport  -h localhost -d openkeywords_development -c keyword_years --csv -f t,c,v > /tmp/years.csv'
+		system 'mongoexport  -h localhost -d openkeywords_development -c keyword_alls   --csv -f t,c,v > public/tmp/all.csv'
+	  system 'mongoexport  -h localhost -d openkeywords_development -c keyword_days   --csv -f t,c,v > public/tmp/days.csv'
+	  system 'mongoexport  -h localhost -d openkeywords_development -c keyword_weeks  --csv -f t,c,v > public/tmp/weeks.csv'
+	  system 'mongoexport  -h localhost -d openkeywords_development -c keyword_months --csv -f t,c,v > public/tmp/months.csv'
+	  system 'mongoexport  -h localhost -d openkeywords_development -c keyword_years  --csv -f t,c,v > public/tmp/years.csv'
 	end	
 	
 end	
